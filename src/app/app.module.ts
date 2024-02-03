68616e2a6772307887a18a4bbfbb3b857632e8b0
Poorsaken <chiyokogaming02@gmail.com>
2024-02-03 13:41:00 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
