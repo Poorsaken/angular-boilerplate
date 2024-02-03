6495f7e406e2aa0ae261abb2878dbdab79a0cfde
Poorsaken <chiyokogaming02@gmail.com>
2024-02-03 13:47:38 +0800

    CSS Change
    changed css

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
-  styleUrls: ['./container.component.css']
+  styleUrls: ['./container.component.css'],
+
+
 })
 export class ContainerComponent {
 
